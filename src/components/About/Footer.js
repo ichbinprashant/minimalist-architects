892a2a49bf424c874f0994665a69dbf3c5ccbff4
ichbinprashant <mishraprashant304@gmail.com>
2022-04-16 17:27:31 +0530

    linking

--- src/components/About/Footer.js
+++ src/components/About/Footer.js
@@ -12,22 +12,22 @@ export default function Footer(){
                         <a href="mailto:theminimalistarchitects@gmail.com"><FaEnvelope /></a>
                     </li>
                     <li className="twitter">
-                        <a href="https://www.twitter.com"><FaTwitter /></a>
+                        <a href="https://twitter.com/thearminimalist"><FaTwitter /></a>
                     </li >
                     <li className="instagram">
                         <a href="https://www.instagram.com/theminimalistarchitects/"><FaInstagram /></a>
                     </li>
                     <li className="linkedin">
-                        <a href="https://www.linkedin.com"><FaLinkedin /></a>
+                        <a href="https://www.linkedin.com/in/the-minimalist-architects-6109a7237/"><FaLinkedin /></a>
                     </li>
                     <li className="facebook">
-                        <a href="https://www.facebook.com/minimalistarchitects"><FaFacebook /></a>
+                        <a href="https://www.facebook.com/The-Minimalist-Architects-110420181641243"><FaFacebook /></a>
                     </li>
                     <li>
-                        <a href=""><FaPinterest /></a>
+                        <a href="https://in.pinterest.com/theminimalistarchitects/_saved/"><FaPinterest /></a>
                     </li>
                     <li>
-                        <a href=""><FaTumblrSquare /></a>
+                        <a href="https://www.tumblr.com/blog/view/theminimalistarchitects"><FaTumblrSquare /></a>
                     </li>
                 </ul>
                 <h6 className="location"><FaMapMarkerAlt />&emsp;Namchi, Sikkim</h6>
--- src/components/About/Under_const.js
+++ src/components/About/Under_const.js
@@ -8,7 +8,7 @@ const Under_const = () => {
           <section></section>
           <div className='under_c'>
             
-            <h2>Site under construction.</h2>
+            <h2>Under construction</h2>
             <br/><br/><br/><br/><br/>
             {/* <a className='email' href="mailto:someone@yoursite.com"><FaEnvelope /> Reach Out To Us</a> */}
         </div>
